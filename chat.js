195697034006b5f9a86e40e8f2615921b1edf271
Arunram Kalaiselvan <arunram@placeiq.com>
2014-12-09 19:24:07 -0500

    leave room only if there is a room to leave

--- chat.js
+++ chat.js
@@ -84,13 +84,16 @@ module.exports = {
                     var roomKey = that.roomKey;
                     var current_room = user[roomKey] || "";
                     if (current_room != roomname) {
-                        that.leaveRoom(user_id, current_room, function(error, status){
-                            room.push(user_id);
-                            that.redisClient.hset(that.roomStore, roomname, JSON.stringify(room));
-                            that.redisClient.hset(that.userStore, user_id, JSON.stringify({"name": user["name"], "socket":user_id, "room":roomname}))
-                            that.redisPubClient.publish("main_chat", JSON.stringify({"message": message, roomKey: roomname, "username": "room_admin"}));
-                            callback(false, true);
-                        });
+                        if (current_room) {
+                            that.leaveRoom(user_id, current_room, function(error, status){
+                            // make user leave room
+                            });
+                        }
+                        room.push(user_id);
+                        that.redisClient.hset(that.roomStore, roomname, JSON.stringify(room));
+                        that.redisClient.hset(that.userStore, user_id, JSON.stringify({"name": user["name"], "socket":user_id, "room":roomname}))
+                        that.redisPubClient.publish("main_chat", JSON.stringify({"message": message, roomKey: roomname, "username": "room_admin"}));
+                        callback(false, true);
                     } else {
                         callback(false, "You are already in this room");
                     }
